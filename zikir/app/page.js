b1595dacfaee3d44e255d3e170a5e166e37e0c91
Rifat Hossain <rifathossain328663@gmail.com>
2026-03-04 01:51:19 +0600

    added shadCn and some styles

--- zikir/app/page.js
+++ zikir/app/page.js
@@ -1,23 +1,41 @@
+"use client";
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import { Plus, Minus } from "lucide-react";
+import Zikir from "@/components/Zikir";
 export default function Home() {
   return (
     <main>
-      <div className="counter">
-        <label> 33</label>
-        <button>+</button>
-        <button>-</button>
+      <div className="counter-wraper flex justify-center items-center">
+        <div className="counter mt-4 w-90 h-90 flex justify-center items-center">
+          <Button
+            variant="destructive"
+            className="rounded-full cursor-pointer w-16 h-16"
+          >
+            <Minus size={32} strokeWidth={2.5} className="size-7" />
+          </Button>
+
+          <h2 className="w-32 h-32 -mx-4 rounded-full z-10 bg-accent flex justify-center items-center text-6xl">
+            {" "}
+            33
+          </h2>
+
+          <Button className="rounded-full cursor-pointer bg-primary w-16 h-16">
+            <Plus strokeWidth={2.5} fill="white" className="size-7" />
+          </Button>
+        </div>
       </div>
+
       <div className="zikirs">
-        <zikir count={0} name="SubhanAllah" />
-        <zikir count={0} name="Alhamdulillah" />
-        <zikir count={0} name="La ilaha illallah" />
-        <zikir count={0} name="Allahu Akbar" />
+        {/* <Zikir count={0} name="SubhanAllah" />
+        <Zikir count={0} name="Alhamdulillah" />
+        <Zikir count={0} name="La ilaha illallah" />
+        <Zikir count={0} name="Allahu Akbar" /> */}
       </div>
       <div className="hadith">
         <h2>Hadith</h2>
         <p>
-          Umhani Bint abu talib , The sister of Ali and the cousin of the
+          Umm hani Bint abu talib , The sister of Ali and the cousin of the
           Prophet (SA), Came to him and said O Prophet of Allah, Now I am Old
           And Weak, tell me about something I can say or do while sitting down.
           The Phropet told her Say 'SubhanAllah' 100 times: It is equivalent to
---
+++ zikir/components/Zikir.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Zikir = () => {
+  return <div>Zikir</div>;
+};
+
+export default Zikir;
